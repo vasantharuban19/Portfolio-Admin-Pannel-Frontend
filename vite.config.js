cc6be22a788746815bf95c674211bf5636b6e0ac
Vasantharuban S <vasanthruban1920@gmail.com>
2025-03-20 12:10:08 +0530

    fix routing issue on vercel

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
 });
