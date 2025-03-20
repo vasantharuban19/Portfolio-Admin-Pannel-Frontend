1759b760039736480c9df742c6719402009c101c
Vasantharuban S <vasanthruban1920@gmail.com>
2025-03-21 01:01:38 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: "/",
   build: {
     outDir: "dist",
@@ -16,8 +21,5 @@ export default defineConfig({
         assetFileNames: "assets/[name]-[hash].[ext]",
       },
     },
-  },
-  server: {
-    historyApiFallback: true,
   },
 });
